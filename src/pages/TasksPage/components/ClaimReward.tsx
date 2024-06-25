4c2d76e136794eb44798fbeaec366c2ddb072190
Maxim <chernykh.dev@gmail.com>
2024-06-25 11:04:55 +0400

    components migration

---
+++ src/pages/TasksPage/components/ClaimReward.tsx
@@ -0,0 +1,13 @@
+export const ClaimReward = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="pb-5">Task Completed Successfully!</div>
+      <a
+        href="claim"
+        className="bg-blue rounded h-10 w-fit px-5 text-center flex items-center justify-center"
+      >
+        Claim Here!
+      </a>
+    </div>
+  );
+};
