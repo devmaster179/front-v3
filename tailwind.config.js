e65105e5ccaa92b8b77f3734b4ed9d08830e4b09
Maxim <chernykh.dev@gmail.com>
2024-06-25 11:04:40 +0400

    components migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        blue: "#3fa9e4",
+      },
+      boxShadow: {
+        buttonGlow: "0 0 10px 0 #3fa9e4",
+      },
+    },
   },
   plugins: [],
 };
