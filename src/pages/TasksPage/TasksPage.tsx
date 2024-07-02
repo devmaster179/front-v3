2b61a6b9d3bbfc044909ba443f7ca32ce79056d2
ILdar Nogmanov <nogmanov@gmail.com>
2024-07-02 13:36:49 +0300

    Get new lootbox from BE

--- src/pages/TasksPage/TasksPage.tsx
+++ src/pages/TasksPage/TasksPage.tsx
@@ -1,17 +1,49 @@
-import { ClaimReward } from "@/pages/TasksPage/components/ClaimReward";
+// @ts-nocheck
+
+// import { ClaimReward } from "@/pages/TasksPage/components/ClaimReward";
 import { TasksList } from "@/pages/TasksPage/components/TaskList";
-import { useState } from "react";
+import { initInitData, initUtils } from "@tma.js/sdk";
+
+import { supabase } from "../../supabase";
 
 export default function TasksPage() {
-  const [hasShared, setHasShared] = useState(false);
+  const initData = initInitData();
+  const utils = initUtils();
+
+  const onShare = async () => {
+    const { data } = await supabase
+      .from("lootboxes")
+      .select()
+      .is("sender_id", null); // get not used lootboxes only
+
+    console.log(data);
+
+    if (!data) return;
+
+    const lootbox = data[Math.floor(Math.random() * data.length)];
+
+    console.log(lootbox);
+
+    console.log(initData);
+
+    await supabase
+      .from("lootboxes")
+      .update({ sender_id: initData.user.id }) // пишем себя сендером = берем лутбокс
+      .eq("id", lootbox.id);
+
+    // TODO stopped here
+    // utils.shareURL(
+    //   import.meta.env.VITE_APP_SUPABASE_URL,
+    //   "Look! Some cool app here!"
+    // );
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      {hasShared ? (
-        <ClaimReward />
-      ) : (
-        <TasksList onShare={(hasShared) => setHasShared(hasShared)} />
-      )}
+      {
+        // hasShared ? <ClaimReward /> :
+        <TasksList onShare={onShare} />
+      }
     </main>
   );
 }
