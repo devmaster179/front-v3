aa7ef2b1777251d721eb81f08f9bc236a95ad794
ILdar Nogmanov <nogmanov@gmail.com>
2024-07-02 14:07:39 +0300

    Try startParam

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,17 @@
 import { Link } from "@/components/Link/Link";
+import { initInitData } from "@tma.js/sdk";
 
 export function HomePage() {
+  const initData = initInitData();
+  console.log(initData?.startParam);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <img src="./lootbox-closed.gif" alt="loading..." />
       <span className="text-center -mt-10 pb-5 ">
         To open this box, you need to fulfill a task
       </span>
+
+      <span>{initData?.startParam}</span>
 
       <Link to="/tasks" className="bg-blue rounded p-2 px-10 text-white">
         Go!
