cb01b847961affb4663a067e1393009bd5386736
ILdar Nogmanov <nogmanov@gmail.com>
2024-07-02 16:32:30 +0300

    Move handler

--- src/pages/TasksPage/TasksPage.tsx
+++ src/pages/TasksPage/TasksPage.tsx
@@ -2,46 +2,13 @@
 
 // import { ClaimReward } from "@/pages/TasksPage/components/ClaimReward";
 import { TasksList } from "@/pages/TasksPage/components/TaskList";
-import { initInitData, initUtils } from "@tma.js/sdk";
-
-import { supabase } from "../../supabase";
 
 export default function TasksPage() {
-  const initData = initInitData();
-  const utils = initUtils();
-
-  const onShare = async () => {
-    const { data } = await supabase
-      .from("lootboxes")
-      .select()
-      .is("sender_id", null); // get not used lootboxes only
-
-    console.log(data);
-
-    if (!data) return;
-
-    const lootbox = data[Math.floor(Math.random() * data.length)];
-
-    console.log(lootbox);
-
-    console.log(initData);
-
-    await supabase
-      .from("lootboxes")
-      .update({ sender_id: initData.user.id }) // пишем себя сендером = берем лутбокс
-      .eq("id", lootbox.id);
-
-    utils.shareURL(
-      `${import.meta.env.VITE_APP_BOT_URL}?startapp=${lootbox.id}`,
-      "Look! Some cool app here!"
-    );
-  };
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       {
         // hasShared ? <ClaimReward /> :
-        <TasksList onShare={onShare} />
+        <TasksList onShare={} />
       }
     </main>
   );
--- src/pages/TasksPage/components/TaskList.tsx
+++ src/pages/TasksPage/components/TaskList.tsx
@@ -1,11 +1,46 @@
+// @ts-nocheck
+
 import { ActionButton } from "@/pages/TasksPage/components/ActionButton";
 import { ActionItem } from "@/pages/TasksPage/components/ActionItem";
+import { initInitData, initUtils } from "@tma.js/sdk";
+
+import { supabase } from "../../../supabase";
 
 interface TasksListProps {
   onShare: () => void;
 }
 
 export const TasksList = ({ onShare }: TasksListProps) => {
+  const initData = initInitData();
+  const utils = initUtils();
+
+  const _onShare = async () => {
+    const { data } = await supabase
+      .from("lootboxes")
+      .select()
+      .is("sender_id", null); // get not used lootboxes only
+
+    console.log(data);
+
+    if (!data) return;
+
+    const lootbox = data[Math.floor(Math.random() * data.length)];
+
+    console.log(lootbox);
+
+    console.log(initData);
+
+    await supabase
+      .from("lootboxes")
+      .update({ sender_id: initData.user.id }) // пишем себя сендером = берем лутбокс
+      .eq("id", lootbox.id);
+
+    utils.shareURL(
+      `${import.meta.env.VITE_APP_BOT_URL}?startapp=${lootbox.id}`,
+      "Look! Some cool app here!"
+    );
+  };
+
   return (
     <>
       <h1 className="-mt-20 pb-5 text-center font-bold text-lg">
@@ -14,7 +49,7 @@ export const TasksList = ({ onShare }: TasksListProps) => {
       <div>
         <ActionItem
           text="1. Share a lootbox with a friend/s"
-          actionButton={<ActionButton onShare={onShare}>Send</ActionButton>}
+          actionButton={<ActionButton onShare={_onShare}>Send</ActionButton>}
         />
         <ActionItem
           text="2. Upload a video with you and your friends"
