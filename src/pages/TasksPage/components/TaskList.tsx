c9647e86bee79845092168b2cc4e4810cd42329b
ILdar Nogmanov <nogmanov@gmail.com>
2024-07-03 13:02:05 +0300

    111

--- src/pages/TasksPage/components/TaskList.tsx
+++ src/pages/TasksPage/components/TaskList.tsx
@@ -32,7 +32,7 @@ export const TasksList = ({ onShare }: TasksListProps) => {
         .from("lootboxes")
         .update({
           sender_id: initData.user.id,
-          parent: initData.startParam, // TODO revert
+          // parent: initData.startParam, // TODO revert
         }) // пишем себя сендером = берем лутбокс
         .eq("id", lootbox.id);
 
