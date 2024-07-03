c5011667306309f8bf043047fbd473e7313b448d
ILdar Nogmanov <nogmanov@gmail.com>
2024-07-03 13:17:03 +0300

    3

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -35,7 +35,12 @@ export function HomePage() {
         .update({ receiver_id: sender_id }) // sender of current lootbox
         .eq("id", parent); // условие - parent lootbox
 
-      // if (!usersLootboxes?.data?.length) return;
+      if (!usersLootboxes?.data?.length) {
+        setLootboxesCount(0);
+        setUSDT(0);
+        setLOOT(0);
+        return;
+      }
 
       setLootboxesCount(usersLootboxes?.data.length);
 
